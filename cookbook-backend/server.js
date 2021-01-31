084151c7964b7c0e5f3d57f0918ac9224d476d48
The-Real-Guy <aasdimaranan@gmail.com>
2021-01-31 00:57:12 -0700

    make it better

--- cookbook-backend/server.js
+++ cookbook-backend/server.js
@@ -16,6 +16,13 @@ app.use(express.json());
 
 app.use('/api/recipes', recipes);
 
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('cookbook/build'));
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'cookbook', 'build', 'index.html')));
+}
+
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server running on ${PORT}`.yellow.bold));
